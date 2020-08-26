095bcb6222f663d918eb15831414268ef40abb09
Conor Fleming <conorfleming03@gmail.com>
2020-08-25 20:58:35 -0700

    practice and excersises

---
+++ playground/jedi2/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+const (
+	con     = "Conor"
+	b   int = 5
+)
+
+const (
+	a = 2017 + iota
+	a2
+	a3
+	a4
+)
+
+func main() {
+	x := 3
+	fmt.Printf("%d\n", x)
+	fmt.Printf("%b\n", x)
+	fmt.Printf("%O\n", x)
+	fmt.Println()
+
+	fmt.Println(con, b)
+	fmt.Println()
+	foo := 12
+	fmt.Printf("%d\n", foo)
+	fmt.Printf("%b\n", foo)
+	fmt.Printf("%#x\n", foo)
+	fmt.Println()
+
+	foo2 := foo << 1
+	fmt.Printf("%d\n", foo2)
+	fmt.Printf("%b\n", foo2)
+	fmt.Printf("%#x\n", foo2)
+
+	lit := `this is a raw lit "string" lol`
+	fmt.Println(lit)
+
+	fmt.Println(a, a2, a3, a4)
+}
--- playground/sandbox/sandbox.go
+++ playground/sandbox/sandbox.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 const (
-	a = iota
-	b
-	c
+	_  = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
 )
 
 func main() {
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
 
+	fmt.Printf("%d\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t%b\n", gb, gb)
 }
