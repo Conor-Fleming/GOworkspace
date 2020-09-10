c209bca19e40a09856595acf6281dff96516f2c5
Conor Fleming <conorfleming03@gmail.com>
2020-09-09 20:03:53 -0700

    directional channels

--- concurrency/channels/01-channels-block/successfull-pass/main.go
+++ concurrency/channels/01-channels-block/successfull-pass/main.go
@@ -6,8 +6,17 @@ func main() {
 	//successful pass example 1
 	c := make(chan int)
 	go func() {
-		c <- 50
+		c <- 42
 	}()
 
 	fmt.Println(<-c)
+
+	//successfull buffer example 2
+	//be weary with buffer channels
+	e := make(chan int, 2)
+
+	e <- 52
+	e <- 55
+	fmt.Println(<-e, <-e)
+
 }
---
+++ concurrency/channels/02-directional-channels/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	//c := make(chan int, 2)
+
+	//c <- 1
+	//c <- 2
+	//fmt.Println(<-c, <-c)
+	fmt.Println("----------")
+	//fmt.Printf("%T\n", c)
+
+	//directional channels
+	c := make(chan int)
+	cr := make(<-chan int) //recieve
+	cs := make(chan<- int) //send
+
+	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", cr)
+	fmt.Printf("%T\n", cs)
+
+	go func() {
+		cs <- 42
+	}()
+
+	fmt.Println(<-cs) //spits an error, send only channel
+
+}
