c205dff69c86a65e19bda1025664941d1499cbbc
Conor Fleming <conorfleming03@gmail.com>
2021-04-23 16:03:21 -0700

    creating Dog package

---
+++ documentation/DogPackage/dog.go
@@ -0,0 +1,7 @@
+//Package dog turns your human years into dog years
+package dog
+
+//Years takes an integer as input and multiplies that input by 7 and returns that value.
+func Years(years int) int {
+	return years * 7
+}
