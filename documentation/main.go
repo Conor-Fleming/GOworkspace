e05b7e7729e9863c6befcc0a22823cfdbffd35b5
Conor Fleming <conorfleming03@gmail.com>
2021-04-23 16:13:40 -0700

    cleanup

---
+++ documentation/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Conor-Fleming/GOworkspace/blob/master/documentation/DogPackage"
+)
+
+func main() {
+	result := DogPackage.Years(25)
+	fmt.Println(result)
+}
