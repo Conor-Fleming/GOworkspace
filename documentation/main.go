4723b02901f72778421ae66a92c5353c54f6ae39
Conor Fleming <conorfleming03@gmail.com>
2021-04-23 17:11:23 -0700

    creating new package and learning dog years

--- documentation/main.go
+++ documentation/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Conor-Fleming/GOworkspace/documentation/DogPackage"
 )
 
 func main() {
-	result := DogPackage.Years(25)
-	fmt.Println(result)
+	fmt.Println("Enter an age you would like converted to Dog years:")
+	var age int
+	_, err := fmt.Scanln(&age)
+	if err != nil {
+		log.Fatalln("That is not a number. Exiting.")
+	}
+
+	result := DogPackage.Years(age)
+	fmt.Println(age, "in dog years is:", result)
 }
